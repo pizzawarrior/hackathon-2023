07288ef4c003c02b7ede8d73bff64da529e29f44
pizzawarrior <i.norstad@gmail.com>
2023-10-19 10:35:55 -0700

    beach names

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,12 @@
 // import React from 'react'
 import { HomeContainer, PictureRow, PictureDiv } from '../assets/style';
-import random from '../assets/random.jpg';
+import Cloudbreak from '../assets/Cloudbreak.jpg';
+import Mavs from '../assets/Mavs.jpg';
+import Nazare from '../assets/Nazare.jpg';
+import Pipeline from '../assets/Pipeline.jpg';
+import PuertoEscondido from '../assets/PuertoEscondido.jpg';
+import Teahupoo from '../assets/Teahupoo.jpg';
+
 
 const Home = () => {
     return (
@@ -8,47 +14,51 @@ const Home = () => {
             <PictureRow>
                 <PictureDiv>
                     <img
-                        src={random}
+                        src={Cloudbreak}
                         alt='beach'
                     />
-                    <div className='bottom'>Beach Name</div>
+                    <div className='bottom'>Cloudbreak</div>
                 </PictureDiv>
+
                 <PictureDiv>
                     <img
-                        src={random}
+                        src={Mavs}
                         alt='beach'
                     />
-                    <div className='bottom'>Beach Name</div>
+                    <div className='bottom'>Mavericks</div>
                 </PictureDiv>
+
                 <PictureDiv>
                     <img
-                        src={random}
+                        src={Nazare}
                         alt='beach'
                     />
-                    <div className='bottom'>Beach Name</div>
+                    <div className='bottom'>Nazare</div>
                 </PictureDiv>
             </PictureRow>
+
             <PictureRow>
                 <PictureDiv>
                     <img
-                        src={random}
+                        src={Pipeline}
                         alt='beach'
                     />
-                    <div className='bottom'>Beach Name</div>
+                    <div className='bottom'>Pipeline</div>
+                </PictureDiv>
+
+                <PictureDiv>
+                    <img
+                        src={PuertoEscondido}
+                        alt='beach'
+                    />
+                    <div className='bottom'>Puerto Escondido</div>
                 </PictureDiv>
                 <PictureDiv>
                     <img
-                        src={random}
+                        src={Teahupoo}
                         alt='beach'
                     />
-                    <div className='bottom'>Beach Name</div>
-                </PictureDiv>
-                <PictureDiv>
-                    <img
-                        src={random}
-                        alt='beach'
-                    />
-                    <div className='bottom'>Beach Name</div>
+                    <div className='bottom'>Tehapuoo</div>
                 </PictureDiv>
             </PictureRow>
         </HomeContainer>
